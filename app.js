0f7e62f9b6ea1451df6405d203adc70f520cb940
FilipSzkarlat <filip.szkarlat@gmail.com>
2025-03-14 17:14:25 +0100

    whole info is displaing on the search page

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ const mainEl = document.querySelector("main");
 let rating = "dupa";
 let runtime = "dupa";
 let genre = "dupa";
+let plot = "dupa";
+let fullPlot = "dupa";
 
 searchBtn.addEventListener("click", getMovie);
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("read-more-info")) {
+    e.target.parentElement.style.display = "none";
+    console.log(
+      (e.target.parentElement.parentElement.querySelector(
+        ".full-plot"
+      ).style.display = "block")
+    );
+  }
+});
 
 async function getMovie() {
   const res = await fetch(
@@ -21,7 +33,7 @@ async function getMovie() {
     await getMoreInfo(element.imdbID);
     mainEl.innerHTML += `
         <div class="movie">
-            <img src="${element.Poster}" alt="${element.Title}" />
+            <img class='poster' src="${element.Poster}" alt="${element.Title}" />
             <div class="movie-info">
                 <div class='title'>
                     <h2>${element.Title}</h2>
@@ -35,6 +47,10 @@ async function getMovie() {
                     <img src='img/plus-icon.png'/>
                     <p>Watchlist</p>
                   </button>
+                </div>
+                <div class='more-info'>
+                  <p class='plot'>${plot}</p>
+                  <p class='full-plot'>${fullPlot}</p>
                 </div>
                 
             </div>
@@ -51,4 +67,12 @@ async function getMoreInfo(imdbID) {
   rating = data.imdbRating;
   runtime = data.Runtime;
   genre = data.Genre;
+  if (data.Plot.length > 100 && window.innerWidth < 1000) {
+    fullPlot = data.Plot;
+    data.Plot =
+      data.Plot.slice(0, 100) +
+      "..." +
+      `<span class='read-more-info'>Read more</span>`;
+  }
+  plot = data.Plot;
 }
